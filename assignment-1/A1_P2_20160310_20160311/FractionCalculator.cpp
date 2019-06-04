250627e896e01a6689ac0a69e89e0192de80bd02
Suleiman99Hesham <45403646+Suleiman99Hesham@users.noreply.github.com>
2019-06-04 20:59:47 +0200

    Add files via upload

---
+++ assignment-1/A1_P2_20160310_20160311/FractionCalculator.cpp
@@ -0,0 +1,6 @@
+#include "FractionCalculator.h"
+
+FractionCalculator::FractionCalculator()
+{
+    //ctor
+}
---
+++ assignment-1/A1_P2_20160310_20160311/FractionCalculator.h
@@ -0,0 +1,13 @@
+#ifndef FRACTIONCALCULATOR_H
+#define FRACTIONCALCULATOR_H
+#include "Fraction.h"
+
+class FractionCalculator
+{
+    public:
+        FractionCalculator();
+        Fraction Frac;
+
+};
+
+#endif // FRACTIONCALCULATOR_H
